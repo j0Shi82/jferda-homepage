b346c0467457e66adde0e6174b50fbf9992300f0
j0Shi82 <janosch.ferda@e-domizil.de>
2020-07-04 23:48:06 +0200

    set overflow on rotating home logos to avoid scrollbar flickering

--- src/components/content/skills/SkillsProgressCat.svelte
+++ src/components/content/skills/SkillsProgressCat.svelte
@@ -16,17 +16,6 @@ export let catIdent;
 export let catOpen = false;
 </script>
 
-<style lang="scss">
-.mdc-typography--headline6 {
-  text-align: center;
-  border-bottom: 1px solid #000;
-  cursor: pointer;
-  justify-content: space-between;
-  align-items: center;
-  display: flex;
-}
-</style>
-
 {#if catOpen}
 <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
   <div class="mdc-layout-grid__inner" in:svelteTransitionSlide="{{ delay: 0, duration: 500 }}">

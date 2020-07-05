49d4d813923e395db9184b287c2c10d2a5f0ad50
j0Shi82 <janosch.ferda@e-domizil.de>
2020-07-05 14:26:09 +0200

    start working on scroll indicator on skills page

--- src/components/content/skills/Skills.svelte
+++ src/components/content/skills/Skills.svelte
@@ -1,15 +1,25 @@
 <script>
 import { localize } from 'utils/imports/core';
-import { svelteTransitionFade } from 'utils/imports/svelte';
+import { svelteTransitionFade, svelteLifecycleOnMount } from 'utils/imports/svelte';
 import { SkillsProgressCat } from 'utils/imports/components';
 import { skillCategories, skillList } from 'utils/imports/data';
 import { routingFadeDuration } from 'utils/imports/config';
-import { MaterialTab, MaterialTabBar, MaterialTabLabel } from 'utils/imports/material';
+import {
+  MaterialTab, MaterialTabBar, MaterialTabLabel, MaterialIcon,
+} from 'utils/imports/material';
 
 import 'assets/style/skills.scss';
 
 const openCat = 'lang';
+let rightArrowOberserver;
+let leftArrowOberserver;
+let showRightArrow = false;
+let showLeftArrow = false;
 let currentCat;
+
+$: {
+  console.log(showLeftArrow, showRightArrow);
+}
 
 // preload images to smoothen transitions
 const promises = [];
@@ -31,16 +41,37 @@ Promise.all(promises).finally(() => {
     });
   }, 250);
 });
+
+svelteLifecycleOnMount(() => {
+  const observerLeft = new IntersectionObserver((entries) => {
+    showLeftArrow = !entries[0].isIntersecting;
+  });
+
+  const observerRight = new IntersectionObserver((entries) => {
+    showRightArrow = !entries[0].isIntersecting;
+  });
+
+  observerLeft.observe(leftArrowOberserver);
+  observerRight.observe(rightArrowOberserver);
+  return () => {
+    observerLeft.unobserve(leftArrowOberserver);
+    observerRight.unobserve(rightArrowOberserver);
+  };
+});
 </script>
 
 <div class="mdc-layout-grid mdc-typography--body1 jdev-route-skills" in:svelteTransitionFade="{{ duration: routingFadeDuration }}">
   <div class="mdc-layout-grid__inner">
     <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+      {#if showLeftArrow}<div class="jdev-left-arrow"><MaterialIcon class="material-icons">keyboard_arrow_left</MaterialIcon></div>{/if}
       <MaterialTabBar tabs={skillCategories} let:tab bind:active="{currentCat}">
+        {#if tab === skillCategories[0]}<div class="jdev-scroll-start" bind:this="{leftArrowOberserver}" />{/if}
         <MaterialTab {tab}>
           <MaterialTabLabel>{$localize(`skills.${tab}`)}</MaterialTabLabel>
         </MaterialTab>
+        {#if tab === [...skillCategories].pop()}<div class="jdev-scroll-end" bind:this="{rightArrowOberserver}" />{/if}
       </MaterialTabBar>
+      {#if showRightArrow}<div class="jdev-right-arrow"><MaterialIcon class="material-icons">keyboard_arrow_right</MaterialIcon></div>{/if}
     </div>
     {#each skillCategories as cat}
         <SkillsProgressCat catIdent="{cat}" catOpen={cat === currentCat} />

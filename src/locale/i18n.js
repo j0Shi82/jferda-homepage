41e1b76a82f82090e94809709c14d5a1218f695d
j0Shi82 <janosch.ferda@e-domizil.de>
2020-07-16 12:54:43 +0200

    package updates

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -3,8 +3,10 @@ import { localeSupportedLanguages, localeStandardLanguage } from 'utils/imports/
 
 export default () => {
   localeSupportedLanguages.forEach((lang) => {
-    const file = `locale/dict/${lang}.json`;
-    registerLocaleDict(lang, () => import(file));
+    // need to disable prefer-template here because of a bug in babel
+    // https://github.com/babel/babel/issues/11844
+    // eslint-disable-next-line prefer-template
+    registerLocaleDict(lang, () => import('locale/dict/' + lang + '.json'));
   });
 
   initLocalization({

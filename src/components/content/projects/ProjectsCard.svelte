cccfcefd9d4c3ce734a3a9dc3cc10d38051fe043
j0Shi82 <janosch.ferda@e-domizil.de>
2022-03-25 22:14:15 +0100

    replace svg masks with inline svgs

--- src/components/content/projects/ProjectsCard.svelte
+++ src/components/content/projects/ProjectsCard.svelte
@@ -28,7 +28,9 @@ export let project;
             {#if hover}
             <CardContent class="jdev-project-hover-content">
                 <span>{$localize(project.descLocaleKey)}</span>
-                <LeadingIcon class="material-icons" style="margin: 5px 0;">star</LeadingIcon>
+                <LeadingIcon class="material-icons" style="margin: 5px 0;">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/></svg>
+                </LeadingIcon>
                 <span class="link" on:click="{() => routerPush(getLocalizedRoute(project.routeName))}">{$localize('projects.learnmore')}</span>
             </CardContent>
             {/if}

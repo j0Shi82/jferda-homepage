eec652eec59959789dc91dafc4e75334934a031a
j0Shi82 <janosch.ferda@e-domizil.de>
2022-03-27 23:54:32 +0200

    fix arrows on skills page that were forgotten when switching from front to svg icons

--- src/components/content/skills/Skills.svelte
+++ src/components/content/skills/Skills.svelte
@@ -66,7 +66,9 @@ svelteLifecycleOnMount(() => {
 <div class="mdc-layout-grid mdc-typography--body1 jdev-route-skills" in:svelteTransitionFade="{{ duration: routingFadeDuration }}">
   <div class="mdc-layout-grid__inner">
     <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-      {#if showLeftArrow}<div class="jdev-arrow jdev-left-arrow"><LeadingIcon class="material-icons">arrow_back_ios</LeadingIcon></div>{/if}
+      {#if showLeftArrow}<div class="jdev-arrow jdev-left-arrow"><LeadingIcon class="material-icons">
+        <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"/></svg>
+      </LeadingIcon></div>{/if}
       <TabBar tabs={skillCategories} let:tab bind:active="{currentCat}">
         {#if tab === skillCategories[0]}<div class="jdev-scroll-start" bind:this="{leftArrowOberserver}" />{/if}
         <Tab {tab}>
@@ -74,7 +76,9 @@ svelteLifecycleOnMount(() => {
         </Tab>
         {#if tab === [...skillCategories].pop()}<div class="jdev-scroll-end" bind:this="{rightArrowOberserver}" />{/if}
       </TabBar>
-      {#if showRightArrow}<div class="jdev-arrow jdev-right-arrow"><LeadingIcon class="material-icons">arrow_forward_ios</LeadingIcon></div>{/if}
+      {#if showRightArrow}<div class="jdev-arrow jdev-right-arrow"><LeadingIcon class="material-icons">
+        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="20px" viewBox="0 0 24 24" width="20px" fill="currentColor"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><polygon points="6.23,20.23 8,22 18,12 8,2 6.23,3.77 14.46,12"/></g></svg>
+      </LeadingIcon></div>{/if}
     </div>
     {#each skillCategories as cat}
         <SkillsProgressCat catIdent="{cat}" catOpen={cat === currentCat} />

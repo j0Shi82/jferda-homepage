ab7b5e897c786ff48e9aad1c09986a2fadff24fe
j0Shi82 <j.ferda@titus.de>
2023-03-03 11:00:54 +0100

    update resume

--- src/config/data/home.js
+++ src/config/data/home.js
@@ -1,5 +1,6 @@
 // store values we need
 import { menuAbout48Icon, menuProjectsIcon, atomTransitionDuration } from 'utils/imports/config';
+import titusCorporateLogo from 'assets/media/images/knowledge-logos/titus.png';
 import vuejsKnowledgeLogo from 'assets/media/images/knowledge-logos/vuejs.png';
 import dockerKnowledgeLogo from 'assets/media/images/knowledge-logos/docker.png';
 import javascriptKnowledgeLogo from 'assets/media/images/knowledge-logos/javascript.png';
@@ -10,57 +11,61 @@ import wpKnowledgeLogo from 'assets/media/images/knowledge-logos/wp.png';
 import gitKnowledgeLogo from 'assets/media/images/knowledge-logos/git.png';
 
 const menuItems = [
-  {
-    icon: menuAbout48Icon,
-    textLocaleIdent: 'navigation.routes.about',
-    route: 'about',
-    transitionDelay: atomTransitionDuration * 0,
-  },
-  {
-    icon: menuProjectsIcon,
-    textLocaleIdent: 'navigation.routes.projects',
-    route: 'projects',
-    transitionDelay: atomTransitionDuration * 1,
-  },
+    {
+        icon: menuAbout48Icon,
+        textLocaleIdent: 'navigation.routes.about',
+        route: 'about',
+        transitionDelay: atomTransitionDuration * 0,
+    },
+    {
+        icon: menuProjectsIcon,
+        textLocaleIdent: 'navigation.routes.projects',
+        route: 'projects',
+        transitionDelay: atomTransitionDuration * 1,
+    },
 ];
 
 menuItems.forEach((item, i) => {
-  menuItems[i].transitionDelay = atomTransitionDuration * i;
+    menuItems[i].transitionDelay = atomTransitionDuration * i;
 });
 
 const knowledgeLogoItems = [
-  {
-    ident: 'vue',
-    logo: vuejsKnowledgeLogo,
-  },
-  {
-    ident: 'svelte',
-    logo: svelteKnowledgeLogo,
-  },
-  {
-    ident: 'javascript',
-    logo: javascriptKnowledgeLogo,
-  },
-  {
-    ident: 'php',
-    logo: phpKnowledgeLogo,
-  },
-  {
-    ident: 'wordpress',
-    logo: wpKnowledgeLogo,
-  },
-  {
-    ident: 'docker',
-    logo: dockerKnowledgeLogo,
-  },
-  {
-    ident: 'node',
-    logo: nodeKnowledgeLogo,
-  },
-  {
-    ident: 'git',
-    logo: gitKnowledgeLogo,
-  },
+    {
+        ident: 'titus',
+        logo: titusCorporateLogo,
+    },
+    {
+        ident: 'vue',
+        logo: vuejsKnowledgeLogo,
+    },
+    {
+        ident: 'svelte',
+        logo: svelteKnowledgeLogo,
+    },
+    {
+        ident: 'javascript',
+        logo: javascriptKnowledgeLogo,
+    },
+    {
+        ident: 'php',
+        logo: phpKnowledgeLogo,
+    },
+    {
+        ident: 'wordpress',
+        logo: wpKnowledgeLogo,
+    },
+    {
+        ident: 'docker',
+        logo: dockerKnowledgeLogo,
+    },
+    {
+        ident: 'node',
+        logo: nodeKnowledgeLogo,
+    },
+    {
+        ident: 'git',
+        logo: gitKnowledgeLogo,
+    },
 ];
 
 export { menuItems, knowledgeLogoItems };
